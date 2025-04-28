8b9bc1aa83f9eba944defe71dfde77c7ed4576b5
jiwon0719 <hjw9265@gmail.com>
2025-04-28 17:13:02 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.13 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        
+        // System.out.println(Arrays.toString(str));
+        
+        return answer;
+    }
+}

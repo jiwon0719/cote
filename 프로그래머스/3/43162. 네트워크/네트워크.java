8bbd3bb2c6356775abf16aee44e0d8430339c05f
황지원 <hjw9265@gmail.com>
2025-06-27 14:07:11 +0900

    [level 3] Title: 네트워크, Time: 0.54 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,29 @@
 import java.io.*;
 import java.util.*;
 
-// 핵심 : BFS가 끝나면 하나의 완전한 네트워크를 찾은 것
-
 class Solution {
     public int solution(int n, int[][] computers) {
+        int answer = 0;
         boolean[] visited = new boolean[n];
-        int networkCount = 0;
         
         for(int i = 0; i < n; i++) {
-            if(!visited[i]){ 
-                bfs(i, computers, visited); 
-                networkCount++;
+            if(!visited[i]) { // 방문하지 않은 컴퓨터를 발견하면
+                bfs(computers, visited, i); // 네트워크 형성
+                answer++; // bfs가 종료되면 하나의 네트워크가 형성된 것
             }
         }
         
-        return networkCount;
-    }
+        return answer;
+    }   
     
-    
-    public void bfs(int start, int[][] computers, boolean[] visited) {
+    public void bfs(int[][] computers, boolean[] visited, int start) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
+        queue.offer(start); 
         visited[start] = true;
         
         while(!queue.isEmpty()) {
             int current = queue.poll();
             
-            // 현재 컴퓨터와 연결되어 있는 모든 컴퓨터 확인
             for(int i = 0; i < computers[current].length; i++) {
                 if(computers[current][i] == 1 && !visited[i]) {
                     queue.offer(i);
@@ -36,5 +32,4 @@ class Solution {
             }
         }
     }
-    
 }

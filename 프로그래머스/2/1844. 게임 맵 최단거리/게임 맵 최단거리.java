c2b38a0ec083fb27941a20683c2bc1f3b162b9a5
황지원 <hjw9265@gmail.com>
2025-06-27 14:17:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.80 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int n = maps.length; 
+        int n = maps.length;
         int m = maps[0].length;
         
         Queue<int[]> queue = new LinkedList<>();
@@ -21,26 +21,23 @@ class Solution {
             int c = current[1];
             int dist = current[2];
             
-            // 진영 도착 시, 거리 리턴
             if(r == n-1 && c == m-1) {
                 return dist;
             }
             
-            // 사방 탐색
             for(int d = 0; d < 4; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
                 
                 if(nr < 0 || nr >= n || nc < 0 || nc >= m 
-                  || visited[nr][nc] || maps[nr][nc] == 0) continue;
+                   || maps[nr][nc] == 0 || visited[nr][nc]) continue;
                 
                 queue.offer(new int[] {nr, nc, dist + 1});
                 visited[nr][nc] = true;
             }
         }
         
-        // 도달 못한 경우 여기 걸림
+        // bfs 끝났는데 리턴 안된건 결국 도착지 못갔음
         return -1;
     }
-    
 }

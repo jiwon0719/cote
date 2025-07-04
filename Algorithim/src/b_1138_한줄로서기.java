def0678236becc1ddfb10522cc95a7ff5f6ff113
황지원 <hjw9265@gmail.com>
2025-07-04 14:23:26 +0900

    수정

---
+++ Algorithim/src/b_1138_한줄로서기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class b_1138_한줄로서기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] input = new int[N+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] output = new int[N];
+        for(int i = 1; i <= N; i++) {
+            int key = i;
+            int currentCnt = 0;
+            int targetCnt = input[i];
+
+            for(int j = 0; j < N; j++) { // output 배열에서 자리 찾아가기
+                if(output[j] != 0) continue;
+
+                // current == target 확인
+                if(currentCnt == targetCnt) {
+                    output[j] = key;
+                    break;
+                } else {
+                    if(output[j] == 0) {
+                      currentCnt++;
+                    }
+                }
+            }
+        }
+
+        for(int n : output) {
+            System.out.print(n + " ");
+        }
+    }
+}

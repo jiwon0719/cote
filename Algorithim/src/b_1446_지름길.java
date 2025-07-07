e242d7cefb2736127e3e6a5911f2b7d647c0628b
황지원 <hjw9265@gmail.com>
2025-07-07 23:04:30 +0900

    [250707]

---
+++ Algorithim/src/b_1446_지름길.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class b_1446_지름길 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int[][] roads = new int[N][3]; // 시작위치, 도착위치, 거리
+        for(int i = 0; i< N; i++) {
+            st = new StringTokenizer(br.readLine());
+            roads[i][0] = Integer.parseInt(st.nextToken());
+            roads[i][1] = Integer.parseInt(st.nextToken());
+            roads[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        // dp 배열 초기화
+        int[] dp = new int[D+1];
+        for(int i = 0; i <= D; i++) {
+            dp[i] = i;
+        }
+
+        for(int i = 0; i <= D; i++) {
+            if(i > 0) {
+                dp[i] = Math.min(dp[i], dp[i-1] + 1);
+            }
+
+            for(int j = 0; j < roads.length; j++) {
+                int start = roads[j][0];
+                int end = roads[j][1];
+                int length = roads[j][2];
+
+                if(i == start && end <= D) {
+                    dp[end] = Math.min(dp[end], dp[i] + length);
+                }
+            }
+        }
+
+        System.out.println(dp[D]);
+    }
+}
---
+++ Algorithim/src/b_1522_문자열교환.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class b_1522_문자열교환 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        char[] arr = str.toCharArray();
+
+        // a 개수 세기
+        int aCnt = 0;
+        for(char c : arr) {
+            if(c == 'a') aCnt++;
+        }
+
+        // 원형 연결을 위해 문자열 두 배로 만들기
+        String doubled = str + str;
+        char[] arr2 = doubled.toCharArray();
+
+        // a 개수 만큼 잘라서 b개수 확인하기
+        int answer = Integer.MAX_VALUE;
+        for(int i = 0; i < str.length(); i++) {
+            int sum = 0;
+            for(int j = i; j < i + aCnt; j++) {
+                if(arr2[j] == 'b') sum++;
+            }
+            answer = Math.min(answer, sum);
+        }
+
+        System.out.println(answer);
+    }
+}

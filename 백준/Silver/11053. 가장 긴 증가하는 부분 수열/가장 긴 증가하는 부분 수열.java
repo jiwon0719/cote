f1afc4dbf45cb340fecda2eb7e6203ccd7bcc57d
황지원 <hjw9265@gmail.com>
2025-08-08 12:36:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        // 점화식 : i번째 숫자가 가장 끝에 위치하는 가장 긴 증가하는 부분 수열의 개수를 저장
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]) dp[i] = Math.max(dp[j] + 1, dp[i]);
+            }
+        }
+        
+        int max = 0;
+        for(int i = 0; i < N; i++) {
+            if(max < dp[i]) max = dp[i];
+        }
+        System.out.println(max);
+    }
+}

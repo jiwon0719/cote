987ce6e284be18089bae1ded3404034a5097ff51
황지원 <hjw9265@gmail.com>
2025-08-12 12:17:31 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16492 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+        char pre = 0;
+        for(char ch : str.toCharArray()) {
+            if(ch == '(') {
+                stack.push(ch);
+            }
+            else if(ch == ')' && pre == '(') {
+                stack.pop();
+                answer += stack.size();
+            }
+            else if(ch == ')' && pre != '(') {
+                stack.pop();
+                answer ++;
+            }
+            pre = ch;
+        }
+
+        System.out.println(answer);
+    }
+}

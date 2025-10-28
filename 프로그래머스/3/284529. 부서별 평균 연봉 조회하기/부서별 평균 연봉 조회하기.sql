02a6c2722cb0bf5363dfa3297748ae86f8b488e8
황지원 <hjw9265@gmail.com>
2025-10-28 21:58:59 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,20 +1,16 @@
--- 코드를 작성해주세요
--- 부서별 평균 연봉 조회
--- 평균 연봉은 소수점 첫째 자리에서 반올림
--- 부서별 평균 연봉으로 내림차순
+-- 부서별로 부서id, 영문부서명, 평균 연봉
+-- 평균 연봉 : 첫째자리 반올림
+-- 평균 연봉 기준 내림차순
 
--- 부서별로 그룹화
--- 연봉 평균구하기
--- 조인해서 부서 정보 얻기
 SELECT
-    E.DEPT_ID, 
-    D.DEPT_NAME_EN, 
-    ROUND(AVG(SAL), 0) AS AVG_SAL
+    E.DEPT_ID,
+    DEPT_NAME_EN,
+    ROUND(AVG(SAL), 0) AS 'AVG_SAL'
 FROM 
     HR_EMPLOYEES E
-JOIN
+LEFT JOIN
     HR_DEPARTMENT D ON E.DEPT_ID = D.DEPT_ID
 GROUP BY
-    E.DEPT_ID
+    E.DEPT_ID, DEPT_NAME_EN
 ORDER BY
     3 DESC;
